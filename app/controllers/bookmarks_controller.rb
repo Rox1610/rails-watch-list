b73c550cac8c02823cff04fd0b33ac54321364fb
Roxanne Devillers <roxannedevillers@gmail.com>
2022-11-26 16:13:16 -0500

    cards done on show page

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,22 +1,22 @@
 class BookmarksController < ApplicationController
+  before_action :set_bookmark, only: :destroy
+  before_action :set_list, only: %i[new create]
+
   def new
     @bookmark = Bookmark.new
-    @list = List.find(params[:list_id])
   end
 
   def create
     @bookmark = Bookmark.new(bookmark_params)
-    @list = List.find(params[:list_id])
     @bookmark.list = @list
     if @bookmark.save
       redirect_to list_path(@list)
     else
-      render 'lists/show'
+      render :new, status: :unprocessable_entity
     end
   end
 
   def destroy
-    @bookmark = Bookmark.find(params[:id])
     @bookmark.destroy
     redirect_to root_path, status: :see_other
   end
@@ -29,5 +29,9 @@ class BookmarksController < ApplicationController
 
   def bookmark_params
     params.require(:bookmark).permit(:comment, :movie_id)
+  end
+
+  def set_list
+    @list = List.find(params[:list_id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root to: "lists#index"
-  resources :lists, except: :index do
-    resources :bookmarks, only: %i[create new]
+  resources :lists, except: %i[edit update] do
+    resources :bookmarks, only: %i[new create]
   end
-  resources :bookmarks, only: %i[destroy]
+  resources :bookmarks, only: :destroy
 end
